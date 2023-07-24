289249bdcc4a3d0eb46dc8af9ba0cd0a99a92fd5
Aditya Kalhan <akalhan_be20@thapar.edu>
2023-07-24 06:03:27 +0530

    data validation step complete

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -9,3 +9,9 @@ class DataIngestionConfig :
     local_data_file : Path
     unzip_dir : Path
 #return types of functions
+
+@dataclass(frozen = True)
+class DataValidationConfig :
+    root_dir : Path
+    STATUS_FILE : str
+    ALL_REQUIRED_FILES : list
